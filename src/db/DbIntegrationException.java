9d66523e18e037d853c6fad11bd93d57a47f8a0b
Eduardo Pereira <eduardo.pereira2162@gmail.com>
2025-01-11 20:34:39 -0300

    Creating project and git repository

---
+++ src/db/DbIntegrationException.java
@@ -0,0 +1,10 @@
+package db;
+
+public class DbIntegrationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DbIntegrationException(String msg) {
+		super(msg);
+	}
+}
