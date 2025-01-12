7084499d588527ca82dd3350030b14ca0fd90d04
Eduardo Pereira <eduardo.pereira2162@gmail.com>
2025-01-12 10:26:04 -0300

    Criação Departamente Entity class

---
+++ src/applcation/Principal.java
@@ -0,0 +1,13 @@
+package applcation;
+
+import model.entities.Department;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+	}
+
+}
