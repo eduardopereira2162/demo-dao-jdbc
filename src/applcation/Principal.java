f20cb1e571613afac9aaf9dbac68ea597f1be452
Eduardo Pereira <eduardo.pereira2162@gmail.com>
2025-01-12 10:53:25 -0300

    SellerDao JDVB e DAo Factory

--- src/applcation/Principal.java
+++ src/applcation/Principal.java
@@ -2,6 +2,8 @@ package applcation;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,12 @@ public class Principal {
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com" ,new Date(), 3000.00, obj);
 	
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println(sellerDao);
+		
+		
 	}
 
 }
---
+++ src/model/dao/mpl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.mpl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findall() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
